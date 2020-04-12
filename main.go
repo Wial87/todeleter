4e4db25e2f5fbf2ddd64f104fd18ca0cda310bda
Wial87 <63091447+Wial87@users.noreply.github.com>
2020-04-12 11:58:59 +0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"deleter/bot"
+)
+
+func main() {
+	/*
+		Вот пример как вбивать аккаунты:
+			accounts := map[string]string{
+			"token": "keyWord",
+			"token": "keyWord",
+		}
+	*/
+
+	accounts := map[string]string{
+		// "token": "keyWord",
+		"3439ba6cce7d15993687e092daf191f5b9b465f81c1741d2f4f67b6c65c04538f6b80ac6daf1a22ff33af": "дд", // Это пример, поля не должны быть пустыми!!!
+	}
+
+	// Функция запуска аккаунтов
+	bot.StartAccounts(accounts)
+}
